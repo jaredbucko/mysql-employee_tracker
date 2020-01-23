37acc466f344b46a46caf829cc5f838156428e0c
jaredbucko <jaredbucko@icloud.com>
2020-01-23 15:57:14 -0800

    updated dependencies and added new query

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
+	CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
---
+++ server.js
@@ -0,0 +1,77 @@
+const mysql = require("mysql");
+const inquirer =require('inquirer');
+const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Gobruins2020",
+  database: "employee_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  startApp();
+});
+
+function startApp() {
+  inquirer
+  .prompt({
+    name: "action",
+    type: "list",
+    message: "Welcome! What would you like to do?",
+    choices: [
+      "Create new department",
+      "Create new role",
+      "Create new employee",
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Update employee roles"
+    ]
+  })
+  .then(function(answer) {
+    switch (answer.action) {
+    case "Create new department":
+      // createDepartment();
+      break;
+
+    case "Create new role":
+      // createRole();
+      break;
+
+    case "Create new employee":
+      // createEmployee();
+      break;
+
+    case "View all departments":
+      // viewDepartments();
+      break;
+
+    case "View all roles":
+      // viewRoles();
+      break;
+
+    case "View all employees":
+    viewEmployees();
+    break;
+    }
+  });
+};
+
+function viewEmployees() {
+  var query = "SELECT * FROM employee";
+  connection.query(query, function(err, res) {
+    console.table(res);
+    startApp();
+  });
+};
+
+
